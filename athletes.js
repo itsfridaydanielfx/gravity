3b9892da3c66773d4fd304899074415b9e965270
itsfridaydaniel <itsfridaydanielfx@gmail.com>
2026-03-07 02:22:16 +0100

    Add new athlete data for Daniel Karkosz

--- athletes.js
+++ athletes.js
@@ -46,5 +46,37 @@ window.ATHLETES = [
     biceps_kg: 50,
     created_at: '2026-03-07T12:00:00Z',
     updated_at: '2026-03-07T12:00:00Z'
-  }
+  },
+  {
+  id: "daniel-karkosz",
+  name: "Daniel Karkosz",
+  photo: "assets/athletes/placeholder-1.svg",
+  bodyweight_kg: 60,
+  pullup_added_weight_kg: 55,
+  dip_added_weight_kg: 55,
+  muscleup_added_weight_kg: 15,
+  bench_kg: 69,
+  squat_kg: 69,
+  deadlift_kg: 69,
+  ohp_kg: 69,
+  biceps_kg: 15,
+  created_at: "2026-03-07T01:21:24.157Z",
+  updated_at: "2026-03-07T01:21:24.157Z"
+},
+  {
+  id: "daniel-karkosz",
+  name: "Daniel Karkosz",
+  photo: "assets/athletes/placeholder-1.svg",
+  bodyweight_kg: 65,
+  pullup_added_weight_kg: 50,
+  dip_added_weight_kg: 80,
+  muscleup_added_weight_kg: 20,
+  bench_kg: 104,
+  squat_kg: 80,
+  deadlift_kg: 100,
+  ohp_kg: 55,
+  biceps_kg: 50,
+  created_at: "2026-03-07T01:21:24.157Z",
+  updated_at: "2026-03-07T01:21:24.157Z"
+},
 ];
